d874090b58609a5725287dd1a33c8af0fdb0375e
gnayuy <gnayuy@gmail.com>
2017-01-04 14:31:02 -0500

    rm findtiff.cmake and findnifti.cmake

--- cmake/FindBioMedicalDataIO.cmake
+++ cmake/FindBioMedicalDataIO.cmake
@@ -20,7 +20,7 @@ FIND_PATH(BioMedicalDataIO_INCLUDE_DIR
     PATHS ${BioMedicalDataIO_SEARCHPATH}
     DOC "The BioMedicalDataIO include directory")
 
-FIND_LIBRARY(BioMedicalDataIO_LIBRARIES
+FIND_LIBRARY(BioMedicalDataIO_LIBRARY
     NAMES biomedicaldataio libbiomedicaldataio
     PATHS
     /usr/lib
@@ -32,6 +32,48 @@ FIND_LIBRARY(BioMedicalDataIO_LIBRARIES
     /opt/lib64
     /opt/local/lib64
     DOC "The BioMedicalDataIO libraries")
+
+FIND_LIBRARY(TIFF_LIBRARY
+    NAMES tiff libtiff
+    PATHS
+    /usr/lib
+    /usr/local/lib
+    /usr/lib64
+    /usr/local/lib64
+    /opt/lib
+    /opt/local/lib
+    /opt/lib64
+    /opt/local/lib64
+    DOC "The TIFF libraries")
+
+FIND_LIBRARY(NIfTI_LIBRARY
+    NAMES niftiio libniftiio
+    PATHS
+    /usr/lib
+    /usr/local/lib
+    /usr/lib64
+    /usr/local/lib64
+    /opt/lib
+    /opt/local/lib
+    /opt/lib64
+    /opt/local/lib64
+    DOC "The NIfTI library")
+
+FIND_LIBRARY(Z_LIBRARY
+    NAMES znz libznz
+    PATHS
+    /usr/lib
+    /usr/local/lib
+    /usr/lib64
+    /usr/local/lib64
+    /opt/lib
+    /opt/local/lib
+    /opt/lib64
+    /opt/local/lib64
+    DOC "The Z library")
+
+SET(NIfTI_LIBRARIES ${NIfTI_LIBRARY} ${Z_LIBRARY})
+SET(BioMedicalDataIO_LIBRARIES ${BioMedicalDataIO_LIBRARY} ${TIFF_LIBRARY} ${NIfTI_LIBRARIES})
 
 IF(BioMedicalDataIO_INCLUDE_DIR AND BioMedicalDataIO_LIBRARIES)
     SET(BioMedicalDataIO_FOUND TRUE)
